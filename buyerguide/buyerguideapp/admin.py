f80422b61b7858536f94a894186042d89e41b8f9
Mike Beasterfeld <mike.beasterfeld@gmail.com>
2012-03-02 06:08:10 -0600

    add join_date in list display, as list filter, and date navigation bar via date_hierarchy

--- buyerguide/buyerguideapp/admin.py
+++ buyerguide/buyerguideapp/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from buyerguide.buyerguideapp.models import Group, Publication
 
 class GroupAdmin(admin.ModelAdmin):
-   list_display = ('name', 'email')
+   list_display = ('name', 'email', 'join_date')
    search_fields = ('name', 'email')
+   list_filter = ('join_date',)
+   date_hierarchy = 'join_date'
 
 admin.site.register(Group, GroupAdmin)
 admin.site.register(Publication)
