32e6349e916b8cd6f2d36e57aedd24907d438e04
Mike Beasterfeld <mike.beasterfeld@gmail.com>
2012-03-01 21:27:58 -0600

    change email field to models.EmailField

--- buyerguide/buyerguideapp/models.py
+++ buyerguide/buyerguideapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Group(models.Model):
    name = models.CharField(max_length=30)
-   email = models.CharField(max_length=50)
+   email = models.EmailField()
 
    def __unicode__(self):
       return self.name
