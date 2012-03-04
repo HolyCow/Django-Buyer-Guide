90393e4ca6fa4f6137d4bbffe9f38595272ecfca
Mike Beasterfeld <mike.beasterfeld@gmail.com>
2012-03-04 10:06:25 -0600

    add buyerguide app index

--- buyerguide/urls.py
+++ buyerguide/urls.py
@@ -8,10 +8,13 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'buyerguide.views.home', name='home'),
     # url(r'^buyerguide/', include('buyerguide.foo.urls')),
+    
+    url(r'^$', 'buyerguideapp.views.index', name='index'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    
     url(r'^admin/', include(admin.site.urls)),
 )
