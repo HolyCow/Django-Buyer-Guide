6ffd8a32237bba4add24da8d0e3a636f596d4e7e
Mike Beasterfeld <mike.beasterfeld@gmail.com>
2012-03-05 12:38:12 -0600

    add RequestContext to get access to STATIC_URL

--- buyerguide/buyerguideapp/views.py
+++ buyerguide/buyerguideapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
-
+from django.template import RequestContext
 
 def index(request):
-    return render_to_response('buyerguideapp/index.html')
+    return render_to_response('buyerguideapp/index.html', 
+                              {},
+                              context_instance=RequestContext(request))
 
