95091d2e21546d30a3f2ad9f3ef40fa79530e919
Mike Beasterfeld <mike.beasterfeld@gmail.com>
2012-03-05 16:16:04 -0600

    output publisher data to index

--- buyerguide/buyerguideapp/views.py
+++ buyerguide/buyerguideapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from buyerguideapp.models import Group, Publication
+
 def index(request):
     return render_to_response('buyerguideapp/index.html', 
-                              {},
+                              { 'publications': Publication.objects.all, },
                               context_instance=RequestContext(request))
 
